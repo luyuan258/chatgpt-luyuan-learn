b1601f9db055c995a874c1f371350ef8c4abf4b0
luyuan <2836744942@qq.com>
2023-07-01 17:16:52 +0800

    ''

--- hack/docker-env-replace.sh
+++ hack/docker-env-replace.sh
@@ -11,7 +11,7 @@ head_scripts=$HEAD_SCRIPTS
 # Secret string for the project. Use for generating signatures for API calls
 public_secret_key=$PUBLIC_SECRET_KEY
 # Set password for site, support multiple password separated by comma. If not set, site will be public
-site_password="666"
+site_password=$SITE_PASSWORD
 # ID of the model to use. https://platform.openai.com/docs/api-reference/models/list
 openai_api_model=$OPENAI_API_MODEL
 
@@ -22,7 +22,7 @@ for file in $(find ./dist -type f -name "*.mjs"); do
   s/({}).HEAD_SCRIPTS/\"$head_scripts\"/g;
   s/({}).PUBLIC_SECRET_KEY/\"$public_secret_key\"/g;
   s/({}).OPENAI_API_MODEL/\"$openai_api_model\"/g;
-  s/process.env.SITE_PASSWORD/\"$site_password\"/g" $file > tmp
+  s/process.env.SITE_PASSWORD/\"666\"/g" $file > tmp
   mv tmp $file
 done
 
